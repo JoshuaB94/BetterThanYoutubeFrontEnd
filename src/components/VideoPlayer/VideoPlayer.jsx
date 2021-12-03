1a0c1515bd3add7430fa1513c498547de85c4beb
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2021-12-03 06:42:59 -0600

    Coded CSS for VideoPlayer component + Added classes and styled VvideoPlayer container along with title and desc

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,11 +4,16 @@ import React from 'react';
 
 const BetterThanYTPlayer = (props) => {
     return (
-        <div>
-            <iframe title="Embedded Youtube Video Player" id="ytplayer" type="text/html" width="740" height="370"
-            src={props.thePlayerVideo} frameborder="0"></iframe>
-            <h1>{props.theVideoTitle}</h1>
-            <p>{props.theVideoDescription}</p>
+        <div className="player-row">
+            <div className="videoContainer">
+                <iframe className="betterVideoPlayer"title="Embedded Youtube Video Player" id="ytplayer" type="text/html" 
+                src={props.thePlayerVideo} frameborder="0"></iframe>
+            </div>
+
+            <div className="videoInfoContainer">
+                <h3 className="betterVideoTitle">{props.theVideoTitle}</h3>
+                <p className="betterVideoDesc">{props.theVideoDescription}</p>
+            </div>
         </div>
     );
 }
